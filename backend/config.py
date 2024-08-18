213c5fe6a7e68e5f7eb742f3bd52f51c9a281741
nitinkr0411 <nitin.kumar@riotinto.com>
2024-08-18 19:15:52 +0000

    changes

--- backend/config.py
+++ backend/config.py
@@ -88,8 +88,8 @@ logging.getLogger("uvicorn.access").addFilter(EndpointFilter())
 
 
 WEBUI_NAME = os.environ.get("WEBUI_NAME", "Open WebUI")
-if WEBUI_NAME != "Open WebUI":
-    WEBUI_NAME += " (Open WebUI)"
+# if WEBUI_NAME != "Open WebUI":
+#     WEBUI_NAME += " (Open WebUI)"
 
 WEBUI_URL = os.environ.get("WEBUI_URL", "http://localhost:3000")
 
